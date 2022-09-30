4ecaa19fe44f22cc98a30cb469519f940167edf8
sheridan <you@example.com>
2022-09-29 20:46:45 -0500

    hw 10

--- NetPay.py
+++ NetPay.py
@@ -1,9 +1,7 @@
 import PayrollDeductionClass as pd
 import EmployeeClass as ec 
 
-
 def net_pay(): 
-
 
     name = 'Jimmy Smith'
     id_1 = 58475
@@ -13,9 +11,7 @@ def net_pay():
 
     employee = ec.Employee(name,id_1,dept,title,salary)
 
-
     table = []
-
 
     table.append(pd.payroll('food court','8/14/2022',22.50,39119))
     table.append(pd.payroll('gift contribution','8/12/2022',25.00,58475))
@@ -26,13 +22,12 @@ def net_pay():
     net_pay = employee.get_salary()
     
     for record in table:
-        if record.get_empID() == employee.get_id():
-            net_pay = net_pay - record.get_chrg()
-
+        if record.get_empID() == employee.get_idNumber():
+            net_pay = net_pay - record.get_charge()
 
     print('*** Employee Pay ***')
     print('Name:',employee.get_name())
-    print('ID Number:',employee.get_id())
+    print('ID Number:',employee.get_idNumber())
     print('Department:',employee.get_department())
     print('Gross Pay:','$' + str(format(employee.get_salary(),',.2f')))
     print('Net Pay:','$' + str(format(net_pay,',.2f')))
